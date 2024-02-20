3aaa504efd75596faa066e98463febacd078b418
Temuujin undefined <23LP5268@G401-CC0096.local>
2024-02-20 15:07:27 +0800

    2

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 // const products = reguire("./dummy.json");
 const { products, users } = require("./dummy.json");
 const app = express();
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+// const fs = require("fs");
 
 // console.log(products);
 // const products = [
@@ -24,7 +27,20 @@ app.get("/products", (reguest, response) => {
   response.send({ products: products });
 });
 
-app.get("/users", (reguest, response) => {
+app.post("add-user", (reguest, response) => {
+  // const newUser = reg.body;
+  // console.log(newUser);
+
+  // fs.writeFile("dummyTest.json", JSON.stringify(newUser), function (err) {
+  //   if (err) throw err;
+  //   console.log("Saved!");
+  // });
+
+  response.status(200);
+  response.send("succes");
+});
+
+app.post("/users", (reguest, response) => {
   response.type = "application/json";
   response.send({ users: users });
 });
